2a44ca334fb2ebf70b20c28e8f5221247e323256
Azra Baltali <baltaliazra03@gmail.com>
2026-06-16 14:38:11 +0300

    palindrom and set

--- data_str/arrayexx.py
+++ data_str/arrayexx.py
@@ -11,3 +11,21 @@ print(palindrome("kayak"))   # True
 print(palindrome("hello"))   # False
 print(palindrome("aba"))     # True
 print(palindrome("abca"))    # False
+
+
+#remove duplicates:
+def remove_duplicates_set(s):
+    seen = set()
+    result = ""
+    
+    for char in s:
+        if char not in seen:
+            seen.add(char)
+            result += char
+    
+    return result
+
+# Test
+print(remove_duplicates_set("programlama"))  # "progaml"
+print(remove_duplicates_set("hello"))        # "helo"
+print(remove_duplicates_set("banana"))       # "ban"
